6824b09463328441f148bdda63344ffbf1770584
Justin M. Keyes <justinkz@gmail.com>
2018-07-13 20:33:22 +0200

    WIP/smurf: zero-friction Press-Enter prompt

--- src/nvim/message.c
+++ src/nvim/message.c
@@ -950,21 +950,22 @@ void wait_return(int redraw)
                        || c == K_DOWN || c == K_PAGEDOWN))
           c = K_IGNORE;
       }
-    } while ((had_got_int && c == Ctrl_C)
-             || c == K_IGNORE
-             || c == K_LEFTDRAG   || c == K_LEFTRELEASE
-             || c == K_MIDDLEDRAG || c == K_MIDDLERELEASE
-             || c == K_RIGHTDRAG  || c == K_RIGHTRELEASE
-             || c == K_MOUSELEFT  || c == K_MOUSERIGHT
-             || c == K_MOUSEDOWN  || c == K_MOUSEUP
-             || (!mouse_has(MOUSE_RETURN)
-                 && mouse_row < msg_row
-                 && (c == K_LEFTMOUSE
-                     || c == K_MIDDLEMOUSE
-                     || c == K_RIGHTMOUSE
-                     || c == K_X1MOUSE
-                     || c == K_X2MOUSE))
-             );
+    } while (0);
+    // } while ((had_got_int && c == Ctrl_C)
+    //          || c == K_IGNORE
+    //          || c == K_LEFTDRAG   || c == K_LEFTRELEASE
+    //          || c == K_MIDDLEDRAG || c == K_MIDDLERELEASE
+    //          || c == K_RIGHTDRAG  || c == K_RIGHTRELEASE
+    //          || c == K_MOUSELEFT  || c == K_MOUSERIGHT
+    //          || c == K_MOUSEDOWN  || c == K_MOUSEUP
+    //          || (!mouse_has(MOUSE_RETURN)
+    //              && mouse_row < msg_row
+    //              && (c == K_LEFTMOUSE
+    //                  || c == K_MIDDLEMOUSE
+    //                  || c == K_RIGHTMOUSE
+    //                  || c == K_X1MOUSE
+    //                  || c == K_X2MOUSE))
+    //          );
     os_breakcheck();
     /*
      * Avoid that the mouse-up event causes visual mode to start.
